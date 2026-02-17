d2d1ab3f5ba2afb0d3520a326148cde0080962a5
Proca <procaclaw@protonmail.com>
2026-02-17 08:36:26 +0000

    update: build script with server IP and auto-run

--- build.bat
+++ build.bat
@@ -14,14 +14,14 @@ echo.
 echo [2/3] Building...
 if not exist "build" mkdir build
 cd build
-cmake .. -G "Visual Studio 17 2022" -A Win32
+cmake -S . -B . -G "Visual Studio 17 2022" -A Win32
 if errorlevel 1 (
     echo ERROR: CMake configuration failed!
     pause
     exit /b 1
 )
 
-cmake --build . --config Release
+cmake --build . -j
 if errorlevel 1 (
     echo ERROR: Build failed!
     pause
@@ -32,6 +32,7 @@ echo.
 echo [3/3] Done!
 echo.
 echo Run the client with:
-echo   .\build\Release\mmorp_client.exe --http-url http://YOUR_SERVER:8080 --ws-url ws://YOUR_SERVER:8080/v1/world/ws
+echo   .\build\Debug\mmorp_client.exe --http-url http://192.168.30.254:8080/ --ws-url ws://192.168.30.254:8080/v1/world/ws
 echo.
+.\build\Debug\mmorp_client.exe --http-url http://192.168.30.254:8080/ --ws-url ws://192.168.30.254:8080/v1/world/ws
 pause
